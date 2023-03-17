3377e0ae77e69c3bc67584ba946b2c48fc0a7dfb
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-03-17 10:50:09 -0500

    [ADD] github Actions

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import "./App.css";
-import { FirstAprouchInformation } from "./components/firstAprouchInformation";
-import { Footer } from "./components/footer";
-import { Header } from "./components/header";
-import { ProjectsList } from "./components/projectsList";
-import { Skills } from "./components/skills";
-import img1 from "./assets/projects-images/1.png";
-import img2 from "./assets/projects-images/2.png";
-import img3 from "./assets/projects-images/3.png";
-
-const array = [
-  {
-    key: 1,
-    img: img1,
-    title: "Todo Machine Whit React Router",
-    description:
-      "Este proyecto es un ToDo machine que usa react router agregar los todos",
-    url: "https://itsmysecondacount.github.io/proyect-todo-platzi-whit-router/#/",
-  },
-  {
-    key: 2,
-    img: img2,
-    title: "Todo Machine Whit Listener",
-    description:
-      "Este es un proyecto de un ToDo machine con un open modal usando el LocalStorage así como tambien agregando un listener para poder saber si se hiso un cambio desde otra pestaña y poder bloquear el add en caso de que suceda",
-    url: "https://itsmysecondacount.github.io/reactProyectFromPlatzi/",
-  },
-  {
-    key: 3,
-    img: img3,
-    title: "Learning State Machines",
-    description:
-      "Aquí aprendí a usar las maquinas de estado con el xstate y se configuró una pasarela para que vallas completando datos así como el consumo de una api para hacer un desplegable con todos los paises que se pueden visitar",
-    url: "https://itsmysecondacount.github.io/learning-xstate/",
-  },
-];
+import './App.css';
+import { FirstAprouchInformation } from './components/firstAprouchInformation';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import { ProjectsList } from './components/projectsList';
+import { Skills } from './components/skills';
+import { arrayProjects as array } from './data';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <FirstAprouchInformation />
-      <Skills />
-      <ProjectsList projectList={array} />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<FirstAprouchInformation />
+			<Skills />
+			<ProjectsList projectList={array} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/components/firstAprouchInformation/index.js
+++ src/components/firstAprouchInformation/index.js
@@ -1,4 +1,5 @@
 import imageReact from '../../assets/real_sprite.png';
+import imagePc from '../../assets/table_art.png';
 import './aprouch.css';
 
 export const FirstAprouchInformation = () => {
@@ -8,14 +9,21 @@ export const FirstAprouchInformation = () => {
 				<h1>Designer, Frontend Developer</h1>
 				<p>I design and code beautifully simple things, and I love what I do.</p>
 				<img className="pixel-art" src={imageReact} alt="pixel-art" />
-				<img src="https://mattfarley.ca/img/hero-devices.svg" alt="copy" />
+				<img src={imagePc} alt="copy" />
 			</div>
 			<div className="small-information-about">
 				<h2>Hi, I'm Jorge. Nice to meet you</h2>
 				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum repudiandae
-					qui, doloremque veniam eius optio molestiae eos magnam officia, atque nulla
-					illum nam odit praesentium magni aperiam obcaecati eveniet distinctio.
+					I'm a designer and frontend developer with experience using React. I'm
+					passionate about creating beautiful and user-friendly websites and applications
+					that help solve problems and make people's lives easier. Whether it's designing
+					a new interface or coding a complex feature, I'm always up for a challenge and
+					love working with others to bring ideas to life. In addition to my design and
+					development skills, I'm also constantly learning about the latest technologies
+					and trends in the field. I believe that staying up-to-date on emerging tools and
+					techniques is essential to delivering the best possible results for clients and
+					users. In my free time, you can find me exploring new design trends, playing
+					video games, or hiking in the great outdoors.
 				</p>
 			</div>
 		</>
